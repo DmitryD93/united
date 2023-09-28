0f0f670950c9267ab85bc86a27ef1521b8bfd994
DmitriyD93 <6imw35b2>
2023-09-28 23:04:31 +0400

    popular

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,30 @@
 // })
 
 
+  
+ 
 
+// tabs
 
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+const tabsItems = document.querySelectorAll('.tabs__item');
 
+tabsBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
 
+        tabsBtn.forEach((item) => {
+            item.classList.remove('tab-active');
+        })
+
+        tabsItems.forEach((item) => {
+            item.classList.remove('tab-active');
+        })
+
+        currentBtn.classList.add('tab-active');
+        currentTab.classList.add('tab-active');
+
+    })
+})
