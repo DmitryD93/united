f383eb621bd87bedd5a258f886d84ca0864270a4
DmitriyD93 <6imw35b2>
2023-10-03 21:55:47 +0400

    update v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,119 @@
-const { src, dest, watch, parallel, series } = require('gulp');
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const svgSprite = require('gulp-svg-sprite');
-const htmlmin = require('gulp-htmlmin');
-const strip = require('gulp-strip-comments');
-const cheerio = require('gulp-cheerio');
-const replace = require('gulp-replace');
-const fileinclude = require('gulp-file-include');
+const { src, dest, watch, parallel, series } = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const svgSprite = require("gulp-svg-sprite");
+const htmlmin = require("gulp-htmlmin");
+const strip = require("gulp-strip-comments");
+const cheerio = require("gulp-cheerio");
+const replace = require("gulp-replace");
+const fileinclude = require("gulp-file-include");
 // const imagemin = require('gulp-imagemin');
 
 function browsersync() {
   browserSync.init({
     server: { baseDir: "app/" },
-    notify: false
+    notify: false,
   });
 }
 
 function styles() {
-  return src('src/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 5 versions'],
-      grid: true
-    }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+  return src([
+    "node_modules/swiper/swiper-bundle.min.css",
+    "src/scss/style.scss",
+  ])
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        grid: true,
+      })
+    )
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-  return src(['src/js/main.js'])
-    .pipe(concat('main.min.js'))
+  return src(["node_modules/swiper/swiper-bundle.min.js", "src/js/main.js"])
+    .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
 function html() {
-  return src(['src/*.html'])
-    .pipe(fileinclude())
-    .pipe(replace('../', ''))
-    .pipe(strip())
-    // .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest('app'))
-    .pipe(browserSync.stream())
-
+  return (
+    src(["src/*.html"])
+      .pipe(fileinclude())
+      .pipe(replace("../", ""))
+      .pipe(strip())
+      // .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(dest("app"))
+      .pipe(browserSync.stream())
+  );
 }
 
 function font() {
-  return src(['src/fonts/*.*'])
-    .pipe(dest('app/fonts'))
-    .pipe(browserSync.stream())
+  return src(["src/fonts/*.*"])
+    .pipe(dest("app/fonts"))
+    .pipe(browserSync.stream());
 }
 
 function images() {
-  return src('src/img/**/*.*')
-    .pipe(dest('app/img'))
-    .pipe(browserSync.stream())
+  return src("src/img/**/*.*").pipe(dest("app/img")).pipe(browserSync.stream());
 }
 
 function svgSprites() {
-  return src('src/img/icons/*.svg')
-    .pipe(cheerio({
-      run: ($) => {
-        $("[fill]").removeAttr("fill");
-        $("[stroke]").removeAttr("stroke");
-        $("[style]").removeAttr("style");
-      },
-      parserOptions: { xmlMode: true },
-    }))
-    .pipe(replace('&gt;', '>'))
-    .pipe(svgSprite({ mode: { stack: { sprite: '../sprite-frontend.svg' } } }))
-    .pipe(dest('src/img'));
+  return src("src/img/icons/*.svg")
+    .pipe(
+      cheerio({
+        run: ($) => {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke");
+          $("[style]").removeAttr("style");
+        },
+        parserOptions: { xmlMode: true },
+      })
+    )
+    .pipe(replace("&gt;", ">"))
+    .pipe(svgSprite({ mode: { stack: { sprite: "../sprite-frontend.svg" } } }))
+    .pipe(dest("src/img"));
 }
 
 function build() {
-  return src([
-      'src/**/*.html',
-      'src/css/style.min.css',
-      'src/js/main.min.js',
-      'src/img/**/*.*',
-    ], {
-      base: 'src'
-    })
-    .pipe(dest('app'))
+  return src(
+    [
+      "src/**/*.html",
+      "src/css/style.min.css",
+      "src/js/main.min.js",
+      "src/img/**/*.*",
+    ],
+    {
+      base: "src",
+    }
+  ).pipe(dest("app"));
 }
 
 function cleanapp() {
-  return del('app')
+  return del("app");
 }
 
 function watching() {
-  watch(['src/**/*.scss'], styles);
-  watch(['src/modules/**/*.scss'], styles);
-  watch(['src/**/*.html'], html);
-  watch(['src/img/**/**/**/*.jpg', 'src/img/**/**/**/*.jpeg', 'src/img/**/**/**/*.png']);
-  watch(['src/img/**/**/**/*.*'], images);
-  watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
-  watch(['src/img/sprites/*.svg'], svgSprites);
+  watch(["src/**/*.scss"], styles);
+  watch(["src/modules/**/*.scss"], styles);
+  watch(["src/**/*.html"], html);
+  watch([
+    "src/img/**/**/**/*.jpg",
+    "src/img/**/**/**/*.jpeg",
+    "src/img/**/**/**/*.png",
+  ]);
+  watch(["src/img/**/**/**/*.*"], images);
+  watch(["src/js/**/*.js", "!src/js/main.min.js"], scripts);
+  watch(["src/img/sprites/*.svg"], svgSprites);
 }
 
 exports.styles = styles;
@@ -115,4 +127,13 @@ exports.font = font;
 exports.cleanapp = cleanapp;
 exports.build = series(cleanapp, images, build, font);
 
-exports.default = parallel(html, font, images, svgSprites, styles, scripts, browsersync, watching)
+exports.default = parallel(
+  html,
+  font,
+  images,
+  svgSprites,
+  styles,
+  scripts,
+  browsersync,
+  watching
+);
--- src/js/main.js
+++ src/js/main.js
@@ -2,37 +2,76 @@
 // const menu = document.querySelector('.mobile-menu')
 // const body = document.body
 
+
+
 // burger.addEventListener('click', () => {
 //   burger.classList.toggle('active')
 //   menu.classList.toggle('active')
 //   body.classList.toggle('lock')
 // })
 
-
-  
- 
-
 // tabs
 
-const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
-const tabsItems = document.querySelectorAll('.tabs__item');
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
 
 tabsBtn.forEach((item) => {
-    item.addEventListener('click', () => {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute('data-tab');
-        let currentTab = document.querySelector(tabId);
+  item.addEventListener("click", () => {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
 
-        tabsBtn.forEach((item) => {
-            item.classList.remove('tab-active');
-        })
+    tabsBtn.forEach((item) => {
+      item.classList.remove("tab-active");
+    });
 
-        tabsItems.forEach((item) => {
-            item.classList.remove('tab-active');
-        })
+    tabsItems.forEach((item) => {
+      item.classList.remove("tab-active");
+    });
 
-        currentBtn.classList.add('tab-active');
-        currentTab.classList.add('tab-active');
+    currentBtn.classList.add("tab-active");
+    currentTab.classList.add("tab-active");
+  });
+});
 
-    })
-})
+// swiper
+
+// Свайпер для блока скидок
+
+
+const discountThumbsContent = document.querySelector(
+  ".discount__swiper-thumbs"
+);
+const discountThumbs = new Swiper(discountThumbsContent, {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+
+const discountSwiperContent = document.querySelector(".discount__swiper");
+
+const discountSwiper = new Swiper(discountSwiperContent, {
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  grabCursor: true,
+
+  navigation: {
+    nextEl: ".discount__slider-btn--next",
+    prevEl: ".discount__slider-btn--prev",
+    clickable: true,
+  },
+
+  pagination: {
+    el: ".discount__swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    // renderBullet: function(index, className) {
+    //   return '<span class="' + className + '">' + (index + 1) + '</span>';
+    // },
+  },
+  thumbs: {
+    swiper: discountThumbs,
+  },
+});
