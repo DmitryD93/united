86ff7a11a0c79b5cb946149af7e630b575d64802
DmitriyD93 <6imw35b2>
2023-10-22 20:00:36 +0400

    адаптив 640 и логика для шапки на низком разрешении

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
-// const burger = document.querySelector('.burger')
-// const menu = document.querySelector('.mobile-menu')
-// const body = document.body
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.header__nav')
+const body = document.body
+const link = document.querySelectorAll('.link')
 
-// burger.addEventListener('click', () => {
-//   burger.classList.toggle('active')
-//   menu.classList.toggle('active')
-//   body.classList.toggle('lock')
-// })
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+  menu.classList.toggle('active')
+  body.classList.toggle('lock')
+
+  link.forEach(e => {
+    e.addEventListener('click', () => {
+      burger.classList.remove('active')
+      menu.classList.remove('active')
+      body.classList.remove('lock')
+    })
+  })
+})
 
 // tabs
 
@@ -101,6 +110,7 @@ const newsSwiper = new Swiper(newsSwiperContent, {
     },
     640: {
       slidesPerView: 2,
+
     },
   },
 
@@ -126,45 +136,88 @@ const newsSwiper = new Swiper(newsSwiperContent, {
 //   });
 // });
 
+
+
 // Фиксированная шапка
+// Обсервер для появления кнопки и плашки поиска
 
 let header = document.querySelector(".header");
 let content = document.querySelector(".hero");
 
-// let observer = new IntersectionObserver(
-//   function (entries) {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         header.classList.remove("fixed");
-//         // search();
-//       } else {
-//         header.classList.add("fixed");
-//       }
-//     });
-//   },
-//   {
-//     threshold: 0.4,
-//   }
-// );
+let observer = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        header.classList.remove("fixed");
+        searchBtn.classList.remove("fixed");
+        search();
+      } else {
+        header.classList.add("fixed");
+        searchBtn.classList.add("fixed");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
 
-// observer.observe(content);
+observer.observe(content);
 
 let searchBtn = document.querySelector(".header__search-btn");
 let headerSearch = document.querySelector(".header__search");
-let closeBtn = document.querySelector(".header__search-close-btn");
+// let closeBtn = document.querySelector(".header__search-close-btn");
 
-// function search() {
-//   searchBtn.addEventListener("click", () => {
-//     headerSearch.classList.add("fixed");
-//   });
-//   if (!header.fixed) {
-//     headerSearch.classList.remove("fixed");
-//   }
-//   closeBtn.addEventListener("click", () => {
-//     headerSearch.classList.remove("fixed");
-//   });
-// }
+function search() {
+  searchBtn.addEventListener("click", () => {
+    headerSearch.classList.add("fixed");
+  });
+  if (!header.fixed) {
+    headerSearch.classList.remove("fixed");
+  };
+  // closeBtn.addEventListener("click", () => {
+  //   headerSearch.classList.remove("fixed");
+  // });
+}
 
-searchBtn.addEventListener("click", () => {
-  headerSearch.classList.toggle("fixed");
-});
+// searchBtn.addEventListener("click", () => {
+//   headerSearch.classList.toggle("fixed");
+// });
+
+
+// Убрать поиск шапки при клике на пустую область
+
+document.addEventListener("click", (e) => {
+    let elem = e.target;
+
+    if(!elem.classList.contains("header__search") && !elem.closest(".fixed")) {
+      headerSearch.classList.remove("fixed"); 
+    }
+
+    //  Убрать блок входа на низком разрешении при клике на пустую область
+
+    if(!elem.classList.contains("header__login-block") && !elem.closest(".header__join-visible-btn")) {
+      loginBlock.classList.remove("active"); 
+    }
+})
+
+// кнопка сброса формы поиска
+
+const searchInput = document.querySelector(".header__search-form-input");
+const clearBtn = document.querySelector(".header__search-clear-btn");
+
+clearBtn.addEventListener("click", () => {
+  searchInput.value = "";
+})
+
+// логика кнопки входа для открытия блока на низком разрешении
+
+const joinDoorBtn = document.querySelector(".header__join-visible-btn");
+const loginBlock = document.querySelector(".header__login-block");
+
+joinDoorBtn.addEventListener("click", () => {
+  loginBlock.classList.toggle("active");
+  headerSearch.classList.remove("fixed");
+})
+
+
